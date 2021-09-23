6789f2ad5ed11e0d7bc7b17ce7ec43261bf44552
dexon03 <89253308+dexon03@users.noreply.github.com>
2021-09-23 10:30:17 +0300

    Add files via upload

---
+++ Lab1/Lab1.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    int a;
+    float S;
+    cin >> a;
+    S = (pow(a, 2) * sqrt(3)) / 4;
+    cout << S;
+}
+
+
---
+++ Lab1/lab1py.py
@@ -0,0 +1,7 @@
+import math
+
+a = int(input())
+
+S = (math.pow(a,2)*math.sqrt(3))/4
+
+print(S)
