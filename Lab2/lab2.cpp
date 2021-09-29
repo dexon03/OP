bf2ac4d6d5c205ce6a4056280f723491e64c5cab
dexon03 <89253308+dexon03@users.noreply.github.com>
2021-09-29 15:40:09 +0300

    Add files via upload

---
+++ Lab2/lab2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <cmath> 
+using namespace std;
+
+int main()
+{
+    float x1, y1, x2, y2, x3, y3, vidst1,vidst2,vidst3;
+    cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
+    vidst1 = sqrt(pow(x1,2) + pow(y1,2));
+    vidst2 = sqrt(pow(x2,2) + pow(y2,2));
+    vidst3 = sqrt(pow(x3,2) + pow(y3,2));
+    if (vidst1 < vidst2 && vidst1 < vidst3) {
+        cout << "first point nearly";
+    }
+    else if (vidst2 < vidst3) {
+        cout << "second point nearly";
+    }
+    else cout << "third point nearly";
+}
+
+
