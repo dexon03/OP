20d413f81efd91c7c44aa24cf94c6018c0c0eace
dexon03 <89253308+dexon03@users.noreply.github.com>
2021-10-20 12:27:19 +0300

    Add files via upload

---
+++ Lab4/Lab4.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	float x, suma, dodanok; 
+	int fact, n;  // n граничне значення лічильника циклу.
+	cin >> x >> n;
+	suma = 1;
+	fact = 1;
+	for (int i = 1; i <= n; i++) {
+		fact *= (2 * i);
+		cout << fact << ' #'<<i;
+		dodanok = pow(-1, i) * (pow(x, 2 * i) / fact);
+		cout << dodanok <<' #' << i ;
+		suma += dodanok;
+		cout << suma << ' #' << i;
+	}
+
+	cout << suma;
+
+}
+
