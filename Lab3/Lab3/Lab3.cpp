1024a33ad68ce512b42b0280b1a1cdcd0549f595
dexon03 <89253308+dexon03@users.noreply.github.com>
2021-10-20 12:28:07 +0300

    Add files via upload

---
+++ Lab3/Lab3/Lab3.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    float x, suma, res;
+    int k, fact, n;
+    cin >> x;
+    n = 0;
+    suma = 1;
+    k = 1;
+    fact = 1;
+    do{
+        fact *= k;
+        suma += pow(x, 2 * k) / (pow(2, k) * fact);
+        res = fabs((pow(x, 2 * k) / (pow(2, k) * fact)) - (pow(x, 2 * (k-1)) / (pow(2, (k-1)) * fact/k)));
+        k++;
+        n++;
+    } while (res > pow(10,-5));
+    cout << "suma = " << suma << "  amount = " << n;
+}
+
